4c2e0930962982e1180601cc01ec04cbdb28e242
meiqua <563714976@qq.com>
2020-10-26 20:01:05 +0800

    fix out of idx

--- fusion.h
+++ fusion.h
@@ -1453,13 +1453,13 @@ public:
     template<typename T>
     void copyToWith0Bound(const cv::Mat& in, cv::Mat& out, const cv::Rect& roi, int rr_ori, int cc_ori){
         for(int r=roi.y, rr=rr_ori; r<roi.y + roi.height; r++, rr++){
-            const T* in_ptr = in.ptr<T>(r);
             T* out_ptr = out.ptr<T>(rr);
             for(int c=roi.x, cc=cc_ori; c<roi.x + roi.width; c++, cc++){
                 if(r<0 || r>=in.rows || c<0 || c>=in.cols){
                     out_ptr[cc] = 0;
                     continue;
                 }else{
+                    const T* in_ptr = in.ptr<T>(r);
                     out_ptr[cc] = in_ptr[c];
                 }
             }
